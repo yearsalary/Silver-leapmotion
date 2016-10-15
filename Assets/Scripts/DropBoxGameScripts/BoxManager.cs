3c4919d8c4db4d9cc9b1d2fdb41c722e66cbcef0
kjw\김재우 <kjw900905@naver.com>
2016-10-16 05:54:02 +0900

    추가

---
+++ Assets/Scripts/DropBoxGameScripts/BoxManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxManager : MonoBehaviour
+{
+
+    public GameObject controlObject;
+    private SetColor component;
+
+    // Use this for initialization
+    void Start()
+    {
+        controlObject = GameObject.Find("Cube");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        component = controlObject.GetComponent<SetColor>();
+        component.SettingColor();
+    }
+}
