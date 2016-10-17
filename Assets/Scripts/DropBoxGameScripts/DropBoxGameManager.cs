553212fbf04a4d4256c0b4376c1b9d763254450d
unknown <kjw900905>
2016-10-18 05:18:43 +0900

    Rigid Hand에 Hand Model Palm Width 값을 조절하면 손크기 조절

--- Assets/Scripts/DropBoxGameScripts/DropBoxGameManager.cs
+++ Assets/Scripts/DropBoxGameScripts/DropBoxGameManager.cs
@@ -1,28 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DropBoxGameManager : MonoBehaviour {
+public class DropBoxGameManager : MonoBehaviour
+{
     public GameObject cubeFactory;
     public GameObject cube;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         MakeBox(6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
 
     void MakeBox(int count)
     {
-        GameObject cube;
-
-        for(int i=0; i<count; i++)
+        for (int i = 0; i < count; i++)
         {
             cube = (GameObject)Instantiate(cube, cubeFactory.transform.position, cube.transform.rotation);
+
+            switch (i % 4)
+            {
+                case 0 :
+                    cube.GetComponent<Renderer>().material.color = Color.yellow;
+                    break;
+                case 1 :
+                    cube.GetComponent<Renderer>().material.color = Color.red;
+                    break;
+                case 2 :
+                    cube.GetComponent<Renderer>().material.color = Color.green;
+                    break;
+                case 3:
+                    cube.GetComponent<Renderer>().material.color = Color.blue;
+                    break;
+            }
+                
         }
     }
 }
