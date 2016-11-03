0f8a310666da6087560c229ba216108d687d47c6
yearsalary <yearsalary@nate.com>
2016-11-04 05:17:28 +0900

    콜리더 수정...

--- Assets/PlayerBar.cs
+++ Assets/PlayerBar.cs
@@ -45,7 +45,7 @@ public class PlayerBar : MonoBehaviour {
 	{ 
 		Vector3 reflectedVector;
 
-		if (col.gameObject.Equals (ball) && isFirstColEnter) {
+		if (col.gameObject.Equals (ball)) {
 			netWorkCtrl.GetComponent<SocketIOController> ().SendBallCollisonMsg ();
 			if(ball.GetComponent<TableHockeyBall>().getMoveDirection() == Vector3.zero)
 				reflectedVector = Vector3.Reflect (col.contacts [0].point, col.contacts [0].normal.normalized);
