00fc5d5b8f8f40cf393231bcd14214dbccc9b358
yearsalary <yearsalary@nate.com>
2016-11-10 20:01:42 +0900

    ......

--- Assets/Scripts/TableHockeyGameScripts/TableHockeySocketIOController.cs
+++ Assets/Scripts/TableHockeyGameScripts/TableHockeySocketIOController.cs
@@ -55,7 +55,7 @@ public class TableHockeySocketIOController: MonoBehaviour {
 			SendBallMoveMSg ();
 
 		if ((Vector3.Distance (playerBar.transform.position, ball.transform.position) <
-			Vector3.Distance (opponentBar.transform.position, ball.transform.position)) && !ballOwner.Equals (name))
+			Vector3.Distance (opponentBar.transform.position, ball.transform.position)) && !ballOwner.Equals (name) && ball.transform.position.z<0f)
 			SendBallOwnerChangeMsg ();
 
 	}
