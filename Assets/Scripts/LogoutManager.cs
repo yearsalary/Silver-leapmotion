859c52290525561a99e96e9c7effb1c845c11887
yearsalary <yearsalary@nate.com>
2016-11-22 01:09:06 +0900

    LogOut Btn 추가..

---
+++ Assets/Scripts/LogoutManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LogoutManager : MonoBehaviour{
+
+	public void Logout() {
+		GameStatusModel.assistant = null;
+		GameStatusModel.trainee = null;
+		SceneManager.LoadScene ("TitleScene");
+	}
+}
