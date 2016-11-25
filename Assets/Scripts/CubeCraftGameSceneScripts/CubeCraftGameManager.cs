b105144f83d5636c3706ec3de99bc102f43d9f88
yearsalary <yearsalary@nate.com>
2016-11-25 10:43:01 +0900

    CubeCraft 추가.. OBJExporterManager 런타임 인에이블 방식으로 수정.

--- Assets/Scripts/CubeCraftGameSceneScripts/CubeCraftGameManager.cs
+++ Assets/Scripts/CubeCraftGameSceneScripts/CubeCraftGameManager.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class CubeCraftGameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public GameObject cube;
+	public GameObject parentObject;
+	public Text progressText;
+
+	void Update() {
+		CreateCube ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void CreateCube() {
+		GameObject rigidHand = GameObject.Find ("RigidHand(Clone)");
+		GrabbingHand grabbingHand;
+		Transform[] transformArray;
+		GameObject childObj;
+
+		if (rigidHand == null)
+			return;
+		
+		grabbingHand = rigidHand.GetComponentInChildren<GrabbingHand> ();
+		transformArray = rigidHand.GetComponentsInChildren<Transform> ();
+		foreach (Transform childTransform in transformArray) {
+			if (childTransform.name.Contains ("palm") && grabbingHand.GetPinchState ().Equals (GrabbingHand.PinchState.kPinched)) {
+				childObj = (GameObject)Instantiate (cube, childTransform.position, Quaternion.LookRotation(Vector3.forward));		
+				childObj.transform.parent = parentObject.transform;
+			}
+		}
+
 	}
+
+	public void Display(float progress) {
+		progressText.text = "Exporting objects... (" + Mathf.Round (progress* 100) + "%)";
+	}
+
 }
